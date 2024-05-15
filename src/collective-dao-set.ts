0ec1a67115ea1cf1bdc0e91c8bb059b0411b0b75
Benjamin Loo <benjamin@BenjamindeMacBook-Pro.local>
2024-05-15 15:35:46 +0800

    updates

--- src/collective-dao-set.ts
+++ src/collective-dao-set.ts
@@ -13,15 +13,13 @@ import {
     ProposalProcessed
 } from "../generated/ColletiveDaoSetProposalAdapterContract/ColletiveDaoSetProposalAdapterContract";
 import { DaoRegistry } from "../generated/ColletiveDaoSetProposalAdapterContract/DaoRegistry";
-import { VintageRaiserManagementContract } from "../generated/VintageDaoSetAdapterContract/VintageRaiserManagementContract";
-import { VintageFundingPoolAdapterContract } from "../generated/VintageDaoSetAdapterContract/VintageFundingPoolAdapterContract";
+import { ColletiveGovernorManagementAdapterContract } from "../generated/ColletiveDaoSetProposalAdapterContract/ColletiveGovernorManagementAdapterContract";
 import {
     CollectiveDaoSetProposalEntity,
-    VintageProposalVoteInfo,
-    VintageDaoInvestorCapacityEntity,
-    VintageInvestorMembershipEntity,
-    VintageGovernorMembershipEntity,
-    VintageVotingInfoEntity
+    CollectiveProposalVoteInfo,
+    CollectiveDaoInvestorCapacityEntity,
+    CollectiveDaoGovernorMembershipEntity,
+    CollectiveDaoVoteConfigEntity
 } from "../generated/schema"
 
 export function handleProposalCreated(event: ProposalCreated): void {
@@ -72,13 +70,13 @@ export function handleProposalExecuted(event: ProposalProcessed): void {
                 proposalState = BigInt.fromI32(daosetContrct.investorCapProposals(event.params.daoAddr, event.params.proposalId).getState());
                 if (proposalState == BigInt.fromI32(2)) {
 
-                    let vintageDaoInvestorCapacityEntity = VintageDaoInvestorCapacityEntity.load(event.params.daoAddr.toHexString());
-                    if (vintageDaoInvestorCapacityEntity) {
+                    let collectiveDaoInvestorCapacityEntity = CollectiveDaoInvestorCapacityEntity.load(event.params.daoAddr.toHexString());
+                    if (collectiveDaoInvestorCapacityEntity) {
                         const MAX_INVESTORS_ENABLE = daoContract.getConfiguration(Bytes.fromHexString("0x69f4ffb3ebcb7809550bddd3e4d449a47e737bf6635bc7a730996643997b0e48"));
                         const MAX_INVESTORS = daoContract.getConfiguration(Bytes.fromHexString("0xecbde689cc6337d29a750b8b8a8abbfa97427b4ac800ab55be2f2c87311510f2"));
-                        vintageDaoInvestorCapacityEntity.enable = MAX_INVESTORS_ENABLE == BigInt.fromI32(1) ? true : false;
-                        vintageDaoInvestorCapacityEntity.capacityAmount = MAX_INVESTORS;
-                        vintageDaoInvestorCapacityEntity.save();
+                        collectiveDaoInvestorCapacityEntity.enable = MAX_INVESTORS_ENABLE == BigInt.fromI32(1) ? true : false;
+                        collectiveDaoInvestorCapacityEntity.capacityAmount = MAX_INVESTORS;
+                        collectiveDaoInvestorCapacityEntity.save();
                     }
                 }
                 break;
@@ -86,17 +84,17 @@ export function handleProposalExecuted(event: ProposalProcessed): void {
                 proposalState = BigInt.fromI32(daosetContrct.governorMembershipProposals(event.params.daoAddr, event.params.proposalId).getState());
 
                 if (proposalState == BigInt.fromI32(2)) {
-                    let vintageGovernorMembershipEntity = VintageGovernorMembershipEntity.load(event.params.daoAddr.toHexString());
-                    if (vintageGovernorMembershipEntity) {
-                        const governorContAddr = daoContract.getAdapterAddress(Bytes.fromHexString("0xd90e10040720d66c9412cb511e3dbb6ba51669248a7495e763d44ab426893efa"));
-                        const governorManagementCont = VintageRaiserManagementContract.bind(governorContAddr);
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_ENABLE = daoContract.getConfiguration(Bytes.fromHexString("0x5b7b19076468102b84afbef323d128866c396a716ce4ce29f84beef8d266f3af"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x23f0530138a805a7a3c83e0d6298d353102473dc25666f75173de90ac5414c82"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_MIN_HOLDING = daoContract.getConfiguration(Bytes.fromHexString("0x4481bb4c8bc94e65f81ab5591f704165aab99eb4197df7acf672022476933f58"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_MIN_DEPOSIT = daoContract.getConfiguration(Bytes.fromHexString("0x38cbcea6ba035c1d1890c83ce99918342ef746d839f9c2e2d4966d30a7e70b34"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_TOKENID = daoContract.getConfiguration(Bytes.fromHexString("0xe33a9c3c87c85b9efd09c180b8fd30ca34326eb72e20fdd832b510971ccf71ae"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_TOKEN_ADDRESS = daoContract.getAddressConfiguration(Bytes.fromHexString("0xaa2cc36fff47c5c104b89a4abef628c92272c0af9c4db27739bc445b2f51e9a2"));
-                        const VINTAGE_GOVERNOR_MEMBERSHIP_NAME = daoContract.getStringConfiguration(Bytes.fromHexString("0xa4b6f581a2d1e8b24bacedf9a91a13c8df6147ffb9d2bd4a770d867d91018da6"));
+                    let collectiveDaoGovernorMembershipEntity = CollectiveDaoGovernorMembershipEntity.load(event.params.daoAddr.toHexString());
+                    if (collectiveDaoGovernorMembershipEntity) {
+                        const governorContAddr = daoContract.getAdapterAddress(Bytes.fromHexString("0x1a4f1390baec30049008138e650571a3c4374eba88116bc89dc192f2f9295efe"));
+                        const governorManagementCont = ColletiveGovernorManagementAdapterContract.bind(governorContAddr);
+
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_ENABLE = daoContract.getConfiguration(Bytes.fromHexString("0x02049446ffbff09ad55d6fdb03c8c6027cf548332d30663e8fdf960abdab86ea"));
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x09f0ac4191623c4a1e481e66bb865f218d9f5bcef4e65b597583a054b10182bc"));
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_MINI_HOLDING = daoContract.getConfiguration(Bytes.fromHexString("0x6bdd30ca7a0b5a78cdfd780fd2f234880048cafa893e9ccbc9f82df25cdc717f"));
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_TOKEN_ADDRESS = daoContract.getAddressConfiguration(Bytes.fromHexString("0x0567bf43e78d815625031c35adddd5d701cb6b913fbad3b625b4f8c00fdc42aa"));
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_TOKEN_ID = daoContract.getConfiguration(Bytes.fromHexString("0x032de4639c5a8edd1ea478b1345b2d031f42ecd2b29442d3080c9ac0545b5f8e"));
+                        const COLLECTIVE_GOVERNOR_MEMBERSHIP_NAME = daoContract.getStringConfiguration(Bytes.fromHexString("0xe8c0cc8a9993875960b545b0c8b4b345a98d03a2c0ddf4b918a5ef119f5ab528"));
 
 
                         let tem: string[] = [];
@@ -107,84 +105,43 @@ export function handleProposalExecuted(event: ProposalProcessed): void {
                             }
                         }
 
-                        vintageGovernorMembershipEntity.daoAddr = event.params.daoAddr;
-                        vintageGovernorMembershipEntity.enable = VINTAGE_GOVERNOR_MEMBERSHIP_ENABLE == BigInt.fromI32(1) ? true : false;
-                        vintageGovernorMembershipEntity.name = VINTAGE_GOVERNOR_MEMBERSHIP_NAME;
-                        vintageGovernorMembershipEntity.tokenAddress = VINTAGE_GOVERNOR_MEMBERSHIP_TOKEN_ADDRESS;
-                        vintageGovernorMembershipEntity.tokenId = VINTAGE_GOVERNOR_MEMBERSHIP_TOKENID;
-                        vintageGovernorMembershipEntity.minAmount = VINTAGE_GOVERNOR_MEMBERSHIP_TYPE == BigInt.fromI32(4) ? VINTAGE_GOVERNOR_MEMBERSHIP_MIN_DEPOSIT : VINTAGE_GOVERNOR_MEMBERSHIP_MIN_HOLDING;
-                        vintageGovernorMembershipEntity.whiteList = tem;
-                        vintageGovernorMembershipEntity.varifyType = VINTAGE_GOVERNOR_MEMBERSHIP_TYPE;
-                        vintageGovernorMembershipEntity.vintageDaoEntity = event.params.daoAddr.toHexString();
-                        vintageGovernorMembershipEntity.save();
+                        collectiveDaoGovernorMembershipEntity.daoAddr = event.params.daoAddr;
+                        collectiveDaoGovernorMembershipEntity.enable = COLLECTIVE_GOVERNOR_MEMBERSHIP_ENABLE == BigInt.fromI32(1) ? true : false;
+                        collectiveDaoGovernorMembershipEntity.name = COLLECTIVE_GOVERNOR_MEMBERSHIP_NAME;
+                        collectiveDaoGovernorMembershipEntity.tokenAddress = COLLECTIVE_GOVERNOR_MEMBERSHIP_TOKEN_ADDRESS;
+                        collectiveDaoGovernorMembershipEntity.tokenId = COLLECTIVE_GOVERNOR_MEMBERSHIP_TOKEN_ID;
+                        collectiveDaoGovernorMembershipEntity.minHolding = COLLECTIVE_GOVERNOR_MEMBERSHIP_MINI_HOLDING;
+                        collectiveDaoGovernorMembershipEntity.whiteList = tem;
+                        collectiveDaoGovernorMembershipEntity.varifyType = COLLECTIVE_GOVERNOR_MEMBERSHIP_TYPE;
+                        collectiveDaoGovernorMembershipEntity.collectiveDaoEntity = event.params.daoAddr.toHexString();
+                        collectiveDaoGovernorMembershipEntity.save();
                     }
                 } break;
-            // case 2:
-            //     proposalState = BigInt.fromI32(daosetContrct.investorMembershipProposals(event.params.daoAddr, event.params.proposalId).getState());
-
-            //     if (proposalState == BigInt.fromI32(2)) {
-            //         let vintageInvestorMembershipEntity = VintageInvestorMembershipEntity.load(event.params.daoAddr.toHexString());
-            //         if (vintageInvestorMembershipEntity) {
-            //             const fundingPoolContractAddress = daoContract.getAdapterAddress(Bytes.fromHexString("0xaaff643bdbd909f604d46ce015336f7e20fee3ac4a55cef3610188dee176c892"));
-            //             const fundingPoolCont = VintageFundingPoolAdapterContract.bind(fundingPoolContractAddress);
-
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_ENABLE = daoContract.getConfiguration(Bytes.fromHexString("0x1405d0156cf64c805704fdf6691baebfcfa0d409ea827c231693ff0581b0b777"));
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x80140cd7e0b1d935bee578a67a41547c82987de8e7d6b3827d411b738110258b"));
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_MIN_HOLDING = daoContract.getConfiguration(Bytes.fromHexString("0x04ecaf460eb9f82aeb70035e3f24c18a3650fa0da9ddbe7e63d70de659b9b901"));
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_TOKENID = daoContract.getConfiguration(Bytes.fromHexString("0x6cb5bc3796b0717ca4ff665886c96fb0178d6341366eb7b6c737fe79083b836a"));
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_TOKEN_ADDRESS = daoContract.getAddressConfiguration(Bytes.fromHexString("0xe373ab56628c86db3f0e36774c2c5e0393f9272ff5c976bc3f0db2db60cdbc14"));
-            //             const VINTAGE_INVESTOR_MEMBERSHIP_NAME = daoContract.getStringConfiguration(Bytes.fromHexString("0x324dfda0ffcc38c4650b5df076e6f7b4938c2b723873af58b1be5e221dd2cc30"));
-
-            //             let tem: string[] = [];
-            //             const whitelist = fundingPoolCont.getInvestorMembershipWhiteList(event.params.daoAddr)
-            //             if (whitelist.length > 0) {
-            //                 for (let j = 0; j < whitelist.length; j++) {
-            //                     tem.push(whitelist[j].toHexString())
-            //                 }
-            //             }
-
-            //             vintageInvestorMembershipEntity.daoAddr = event.params.daoAddr;
-            //             vintageInvestorMembershipEntity.enable = VINTAGE_INVESTOR_MEMBERSHIP_ENABLE == BigInt.fromI32(1) ? true : false;
-            //             vintageInvestorMembershipEntity.name = VINTAGE_INVESTOR_MEMBERSHIP_NAME;
-            //             vintageInvestorMembershipEntity.minAmount = VINTAGE_INVESTOR_MEMBERSHIP_MIN_HOLDING;
-            //             vintageInvestorMembershipEntity.tokenAddress = VINTAGE_INVESTOR_MEMBERSHIP_TOKEN_ADDRESS;
-            //             vintageInvestorMembershipEntity.tokenId = VINTAGE_INVESTOR_MEMBERSHIP_TOKENID;
-            //             vintageInvestorMembershipEntity.varifyType = VINTAGE_INVESTOR_MEMBERSHIP_TYPE;
-            //             vintageInvestorMembershipEntity.whiteList = tem;
-            //             vintageInvestorMembershipEntity.vintageDaoEntity = event.params.daoAddr.toHexString();
-            //             vintageInvestorMembershipEntity.save();
-            //         }
-            //     } break;
             case 3:
                 proposalState = BigInt.fromI32(daosetContrct.votingProposals(event.params.daoAddr, event.params.proposalId).getState());
 
                 if (proposalState == BigInt.fromI32(2)) {
-                    const vintageVotingInfoEntity = VintageVotingInfoEntity.load(event.params.daoAddr.toHexString());
-                    if (vintageVotingInfoEntity) {
-                        const VINTAGE_VOTING_ASSET_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x686efe7bd1699b408d306db6bbee658ed667971c52d48d6912d7ee496e36e627"));
-                        const VINTAGE_VOTING_ASSET_TOKEN_ADDRESS = daoContract.getAddressConfiguration(Bytes.fromHexString("0xcd9b30ab6388c165d825b60b7d393528191ba59d975b4b1b52b7184b63b8a97c"));
-                        const VINTAGE_VOTING_ASSET_TOKEN_ID = daoContract.getConfiguration(Bytes.fromHexString("0x00634460a4b60c2e1bf8c87bfc42b6b68fd2a71f4bb2d760816eabc5038b5036"));
-                        const VINTAGE_VOTING_WEIGHTED_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0xb75eae231d9582c6afc6491273df4a0ffbccd48ab2c48dbce59e5d68f2d19dc4"));
-                        const VINTAGE_VOTING_SUPPORT_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x561bc86bc227a0f760218574791a84f180f6f9f02be9519a1f99c14fce07a310"));
-                        const VINTAGE_VOTING_QUORUM_TYPE = daoContract.getConfiguration(Bytes.fromHexString("0x8097540b5ab8a4c18e0b10cfbfd2c69e65e8da78f4f0485467006bafa3935964"));
-                        const QUORUM = daoContract.getConfiguration(Bytes.fromHexString("0x0324de13a5a6e302ddb95a9fdf81cc736fc8acee2abe558970daac27395904e7"));
-                        const SUPPORT = daoContract.getConfiguration(Bytes.fromHexString("b4c601c38beae7eebb719eda3438f59fcbfd4c6dd7d38c00665b6fd5b432df32"));
-                        const VOTING_PERIOD = daoContract.getConfiguration(Bytes.fromHexString("0x9876c0f0505bfb2b1c38d3bbd25ba13159172cd0868972d76927723f5a9480fc"));
-                        const PROPOSAL_EXECUTE_DURATION = daoContract.getConfiguration(Bytes.fromHexString("0x02a3530cbb6e7c084516c86f68bd62c3e3fcd783c6c5d7e138616207f7a32250"));
+                    const collectiveDaoVoteConfigEntity = CollectiveDaoVoteConfigEntity.load(event.params.daoAddr.toHexString());
+                    if (collectiveDaoVoteConfigEntity) {
 
-                        vintageVotingInfoEntity.daoAddr = event.params.daoAddr;
-                        vintageVotingInfoEntity.executingPeriod = PROPOSAL_EXECUTE_DURATION;
-                        vintageVotingInfoEntity.quorum = QUORUM;
-                        vintageVotingInfoEntity.quorumType = VINTAGE_VOTING_QUORUM_TYPE;
-                        vintageVotingInfoEntity.support = SUPPORT;
-                        vintageVotingInfoEntity.supportType = VINTAGE_VOTING_SUPPORT_TYPE;
-                        vintageVotingInfoEntity.tokenAddress = VINTAGE_VOTING_ASSET_TOKEN_ADDRESS;
-                        vintageVotingInfoEntity.tokenID = VINTAGE_VOTING_ASSET_TOKEN_ID;
-                        vintageVotingInfoEntity.votingAssetType = VINTAGE_VOTING_ASSET_TYPE;
-                        vintageVotingInfoEntity.votingPeriod = VOTING_PERIOD;
-                        vintageVotingInfoEntity.votingWeightedType = VINTAGE_VOTING_WEIGHTED_TYPE;
-                        vintageVotingInfoEntity.vintageDaoEntity = event.params.daoAddr.toHexString();
-                        vintageVotingInfoEntity.save();
+                        const votingPeriod = daoContract.getConfiguration(Bytes.fromHexString("0x9876c0f0505bfb2b1c38d3bbd25ba13159172cd0868972d76927723f5a9480fc"));
+                        const support = daoContract.getConfiguration(Bytes.fromHexString("0xb4c601c38beae7eebb719eda3438f59fcbfd4c6dd7d38c00665b6fd5b432df32"));
+                        const votingAsset = daoContract.getConfiguration(Bytes.fromHexString("0xeee23dc9ab95b6666db01c2b6cae6a5ef706099a25926e21e0a2e043fe885604"));
+                        const weightAlgorithm = daoContract.getConfiguration(Bytes.fromHexString("0xd093d4a34a12a221b19c0a6689d5449f1346aa769d15cca4e9782c36fda9339a"));
+                        const quorum = daoContract.getConfiguration(Bytes.fromHexString("0x0324de13a5a6e302ddb95a9fdf81cc736fc8acee2abe558970daac27395904e7"));
+                        const supportType = daoContract.getConfiguration(Bytes.fromHexString("0x1aeed2c0ea31fb81f41489b79a1d09374a270e0f9340ace7354cce77586a7d16"));
+                        const quorumType = daoContract.getConfiguration(Bytes.fromHexString("0e0434138ea29dceb1f2c0ca9b9f923f9d1c32d3d96bab2de9991f140daca712"));
+
+                        collectiveDaoVoteConfigEntity.daoAddr = event.params.daoAddr;
+                        collectiveDaoVoteConfigEntity.quorum = quorum;
+                        collectiveDaoVoteConfigEntity.quorumType = quorumType;
+                        collectiveDaoVoteConfigEntity.support = support;
+                        collectiveDaoVoteConfigEntity.supportType = supportType;
+                        collectiveDaoVoteConfigEntity.votingAsset = votingAsset;
+                        collectiveDaoVoteConfigEntity.votingPeriod = votingPeriod;
+                        collectiveDaoVoteConfigEntity.weightAlgorithm = weightAlgorithm;
+                        collectiveDaoVoteConfigEntity.collectiveDaoEntity = event.params.daoAddr.toHexString();
+                        collectiveDaoVoteConfigEntity.save();
                     }
                 } break;
             default:
@@ -195,12 +152,12 @@ export function handleProposalExecuted(event: ProposalProcessed): void {
         entity.save();
     }
 
-    let voteInfoEntity = VintageProposalVoteInfo.load(event.params.proposalId.toHexString());
+    let collectiveProposalVoteInfo = CollectiveProposalVoteInfo.load(event.params.proposalId.toHexString());
 
-    if (voteInfoEntity) {
-        voteInfoEntity.nbYes = event.params.nbYes;
-        voteInfoEntity.nbNo = event.params.nbNo;
-        voteInfoEntity.totalWeights = event.params.allVotingWeight;
-        voteInfoEntity.save();
+    if (collectiveProposalVoteInfo) {
+        collectiveProposalVoteInfo.nbYes = event.params.nbYes;
+        collectiveProposalVoteInfo.nbNo = event.params.nbNo;
+        collectiveProposalVoteInfo.totalWeights = event.params.allVotingWeight;
+        collectiveProposalVoteInfo.save();
     }
 }
