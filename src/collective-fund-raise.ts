70845b680a70358b813a22042dd0c265a74390b7
Benjamin Loo <benjamin@BenjamindeMacBook-Pro.local>
2024-05-29 08:59:12 +0800

    update

--- src/collective-fund-raise.ts
+++ src/collective-fund-raise.ts
@@ -15,49 +15,24 @@ export function handleProposalCreated(event: ProposalCreated): void {
     const contract = ColletiveFundRaiseProposalAdapterContract.bind(event.address);
     const rel = contract.try_proposals(event.params.daoAddr, event.params.proposalId);
     if (!rel.reverted) {
-        rel.value.getCreationTime();
-        rel.value.getFundInfo().maxCap;
-        rel.value.getFundInfo().maxDeposit;
-        rel.value.getFundInfo().miniDeposit;
-        rel.value.getFundInfo().miniTarget;
-        rel.value.getFundInfo().tokenAddress;
-
-        rel.value.getFundRaiseType();
-        rel.value.getPriorityDepositor().enable;
-        rel.value.getPriorityDepositor().miniHolding;
-        rel.value.getPriorityDepositor().tokenAddress;
-        rel.value.getPriorityDepositor().tokenId;
-        rel.value.getPriorityDepositor().valifyType;
-        rel.value.getPriorityDepositor().whitelist;
-
-        rel.value.getState();
-        rel.value.getStopVoteTime();
-        rel.value.getTimeInfo().endTime;
-        rel.value.getTimeInfo().startTime;
-
         entity.acceptTokenAddr = rel.value.getFundInfo().tokenAddress;
-
         entity.fundRaiseTarget = rel.value.getFundInfo().miniTarget;
         entity.fundRaiseTargetFromWei = entity.fundRaiseTarget.div(BigInt.fromI32(10 ** 18)).toString();
-        // entity.fundRaiseMaxAmount: BigInt! # uint256
-        // entity.fundRaiseMaxAmountFromWei: String!
+        entity.fundRaiseMaxAmount = rel.value.getFundInfo().maxCap;
+        entity.fundRaiseMaxAmountFromWei = entity.fundRaiseMaxAmount.div(BigInt.fromI32(10 ** 18)).toString();
         entity.lpMinDepositAmount = rel.value.getFundInfo().miniDeposit;
         entity.lpMinDepositAmountFromWei = entity.lpMinDepositAmount.div(BigInt.fromI32(10 ** 18)).toString();
         entity.lpMaxDepositAmount = rel.value.getFundInfo().maxDeposit;
         entity.lpMaxDepositAmountFromWei = entity.lpMaxDepositAmount.div(BigInt.fromI32(10 ** 18)).toString();
         entity.fundRaiseStartTime = rel.value.getTimeInfo().startTime;
         entity.fundRaiseEndTime = rel.value.getTimeInfo().endTime;
-
         entity.priorityDepositEnable = rel.value.getPriorityDepositor().enable;
-
         entity.priorityDepositType = BigInt.fromI32(rel.value.getPriorityDepositor().valifyType);
-
         entity.priorityDepositTokenAddress = rel.value.getPriorityDepositor().tokenAddress;
-
         entity.priorityDepositTokenId = rel.value.getPriorityDepositor().tokenId;
-
         entity.priorityDepositAmount = rel.value.getPriorityDepositor().miniHolding;
-
+        entity.totalFund = BigInt.fromI32(0);
+        entity.totalFundFromWei = "0";
         let tem: string[] = [];
         if (rel.value.getPriorityDepositor().whitelist.length > 0
         ) {
@@ -65,24 +40,16 @@ export function handleProposalCreated(event: ProposalCreated): void {
                 tem.push(rel.value.getPriorityDepositor().whitelist[i].toHexString());
             }
         }
+
         entity.priorityDepositWhiteList = tem;
         entity.state = BigInt.fromI32(rel.value.getState());
-
         entity.creationTime = event.block.timestamp;
         entity.stopVoteTime = rel.value.getStopVoteTime();
-
-        // entity.fundStartTime: BigInt! # uint256
-        // entity.fundEndTime: BigInt! # uint256
-        // entity.totalFund: BigInt! # uint256
-        // entity.totalFundFromWei: String!
-        // entity.fundRaiseType: BigInt # uint256
-        // entity.fundRaiseTypeInString: String
-
+        entity.fundRaiseType = BigInt.fromI32(rel.value.getFundRaiseType());
+        entity.fundRaiseTypeInString = rel.value.getFundRaiseType() == 0 ? "FCFS" : "Free In";
         entity.daoAddr = event.params.daoAddr;
         entity.proposer = event.transaction.from;
         entity.proposalId = event.params.proposalId;
-        entity.creationTime = event.block.timestamp;
-        entity.stopVoteTime = rel.value.getStopVoteTime();
         entity.executeHash = Bytes.empty();
         entity.collectiveDaoEntity = event.params.daoAddr.toHexString();
         entity.save();
@@ -103,8 +70,6 @@ export function handlerProposalProcessed(event: proposalExecuted): void {
             collectiveDaoEntity.save();
         }
     }
-
-
 
     let voteInfoEntity = CollectiveProposalVoteInfo.load(event.params.proposalId.toHexString());
 
