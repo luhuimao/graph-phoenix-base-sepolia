0fc7e8cf95c7eea1cae1b51736636ec09090c46a
Benjamin Loo <benjamin@BenjamindeMacBook-Pro.local>
2024-05-31 09:54:10 +0800

    update

--- src/collective-funding.ts
+++ src/collective-funding.ts
@@ -16,32 +16,30 @@ export function handleProposalCreated(event: ProposalCreated): void {
     const contract = ColletiveFundingProposalAdapterContract.bind(event.address);
     const rel = contract.try_proposals(event.params.daoAddr, event.params.proposalId);
     if (!rel.reverted) {
+
+        entity.daoAddr = event.params.daoAddr;
+        entity.proposalId = event.params.proposalId;
         entity.approver = rel.value.getEscrowInfo().approver
         entity.escrow = rel.value.getEscrowInfo().escrow
         entity.paybackAmount = rel.value.getEscrowInfo().paybackAmount
         entity.paybackToken = rel.value.getEscrowInfo().paybackToken
         entity.price = rel.value.getEscrowInfo().price
-
         entity.executeBlockNum = rel.value.getExecuteBlockNum()
-
         entity.investmentAmount = rel.value.getFundingInfo().fundingAmount
         entity.receiver = rel.value.getFundingInfo().receiver
         entity.token = rel.value.getFundingInfo().token
         entity.totalAmount = rel.value.getFundingInfo().totalAmount
-
         entity.proposer = rel.value.getProposer()
-
         entity.state = BigInt.fromI32(rel.value.getState())
-
         entity.startVotingTime = rel.value.getTimeInfo().startVotingTime
         entity.stopVotingTime = rel.value.getTimeInfo().stopVotingTime
-
         entity.cliffEndTime = rel.value.getVestingInfo().cliffEndTime
         entity.cliffVestingAmount = rel.value.getVestingInfo().cliffVestingAmount
         entity.vestingEndTime = rel.value.getVestingInfo().endTime
         entity.vestingStartTime = rel.value.getVestingInfo().startTime
         entity.vestingInterval = rel.value.getVestingInfo().vestingInterval
         entity.executeHash = Bytes.empty();
+        entity.creationTime = event.block.timestamp;
         entity.collectiveDaoEntity = event.params.daoAddr.toHexString();
         entity.save();
     }
